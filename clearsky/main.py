21f1370685d14628420cf45b36d352a3e3b62376
Bennet Meyers <bennetm@stanford.edu>
2018-03-12 22:14:56 -0700

    better plotting functionality

--- clearsky/main.py
+++ clearsky/main.py
@@ -40,7 +40,7 @@ class StatisticalClearSky(object):
         else:
             return self.data[:, day], self.U[:, :n].dot(np.diag(self.D[:n])).dot(self.P[:n, day])
 
-    def make_clearsky_model(self, n=5, mu1=3.5, eta=1.5, mu2=3, plot=False, return_fits=False, env_fit=0):
+    def make_clearsky_model(self, n=5, mu1=3.5, eta=1.5, mu2=3, deg_terms=False, plot=False, return_fits=False, env_fit=0):
         if self.U is None:
             self.get_eigenvectors()
         daily_scale_factors = ((np.diag(self.D).dot(self.P[:288])))
@@ -48,7 +48,7 @@ class StatisticalClearSky(object):
         fits = np.empty((n, self.P.shape[1]))
         ind = env_fit
         signal = daily_scale_factors[ind, :]
-        fit = envelope_fit(signal, mu=10 ** mu1, eta=10 ** eta, kind='lower', period=365)
+        fit = envelope_fit(signal, mu=10 ** mu1, eta=10 ** eta, kind='lower', period=365, linear_term=deg_terms)
         mask = np.abs(signal - fit) < 1.5
         signals[ind] = signal
         fits[ind, :] = fit
@@ -56,33 +56,52 @@ class StatisticalClearSky(object):
             if ind != env_fit:
                 signal = daily_scale_factors[ind, :]
                 mu_i = mu2
-                fit = masked_smooth_fit_periodic(signal, mask, 365, mu=10**mu_i)
+                fit = masked_smooth_fit_periodic(signal, mask, 365, mu=10**mu_i, linear_term=deg_terms)
                 signals[ind] = signal
                 fits[ind, :] = fit
-        self.DP_clearsky = fits[:, :365]
+        self.DP_clearsky = fits
         self.cleardays = np.arange(self.P.shape[1])[mask]
         if plot:
-            fig, axes = plt.subplots(nrows=n, figsize=(8,n*3), sharex=True)
-            try:
-                for ind in xrange(n):
-                    axes[ind].plot(signals[ind], linewidth=1)
-                    axes[ind].plot(fits[ind], linewidth=1, color='r')
-                    axes[ind].set_title('Daily scale factors for singular vector {}'.format(ind+1))
-                    axes[ind].scatter(np.nonzero(mask), signals[ind][mask], marker='.', color='orange',
-                                      alpha=0.7, s=100)
-                axes[ind].set_xlabel('Day Number')
-                plt.tight_layout()
-                return fig, axes
-            except TypeError:
-                axes.plot(signals[0], linewidth=1)
-                axes.plot(fits[0], linewidth=1, color='r')
-                axes.set_xlabel('Day Number')
-                axes.set_title('Daily scale factors for singular vector 1')
-                axes.scatter(np.nonzero(mask), signals[ind][mask], marker='.', color='orange',
-                             alpha=0.7, s=100)
-                return fig, axes
+            pass
         if return_fits:
-            return signals, fits
+            pass
+        return
+
+    def plot_fits(self, selection=None):
+        '''
+
+        :param selection:   if None     --> plot first daily signal
+                            if int      --> plot top n daily signals
+                            if iterable --> plot daily signals at indices in interable
+        :return:
+        '''
+        if selection is None:
+            iterable = [0]
+            num_fits = 1
+        else:
+            try:
+                iterable = xrange(int(selection))
+                num_fits = int(selection)
+            except TypeError:
+                iterable = iter(selection)
+                num_fits = len(selection)
+        fig, axes = plt.subplots(nrows=num_fits, figsize=(8, num_fits * 3), sharex=True)
+        if num_fits == 1:
+            axes = [axes]
+        signals = np.diag(self.D).dot(self.P[:288])
+        fits = self.DP_clearsky
+        for ix1, ix2 in enumerate(iterable):
+            axes[ix1].plot(signals[ix2], linewidth=1)
+            axes[ix1].plot(fits[ix2], linewidth=1, color='r')
+            axes[ix1].set_title('Daily scale factors for singular vector {}'.format(xrange(fits.shape[0])[ix2] + 1))
+            axes[ix1].scatter(self.cleardays, signals[ix2][self.cleardays], marker='.', color='orange',
+                              alpha=0.7, s=100)
+        axes[ix1].set_xlabel('Day Number')
+        axes[ix1].set_xlim(0, signals.shape[1])
+        plt.tight_layout()
+        if num_fits == 1:
+            axes = axes[0]
+        return fig, axes
 
     def estimate_clearsky(self, day_slice, n=None):
         '''
