0072e64b5f89fb8b3998a8e929878c40b3445b37
Bennet Meyers <bennetm@stanford.edu>
2018-03-14 12:18:09 -0700

    New function for periodic envelope fit with multiplicative deg term.

--- clearsky/utilities.py
+++ clearsky/utilities.py
@@ -62,6 +62,52 @@ def envelope_fit(signal, mu, eta, linear_term=False, kind='upper', period=None):
             signal *= -1
             return -envelope.value.A1, -beta.value
 
+def envelope_fit_with_deg(signal, period, mu, eta, kind='upper', eps=1e-4):
+    '''
+    Perform an approximately periodic envelope fit of a signal.
+    See: https://en.wikipedia.org/wiki/Envelope_(waves)
+    :param signal: The signal to be fit
+    :param mu: A parameter to control the overall stiffness of the fit
+    :param eta: A parameter to control the permeability of the envelope. A large value result in
+    no data points outside the fitted envelope
+    :param kind: 'upper' or 'lower'
+    :return: An envelope signal of the same length as the input
+    '''
+    offset_eps = False
+    if kind == 'lower':
+        signal *= -1
+    if np.min(signal) < 0:
+        print 'function only works for signals with all values >= 0'
+    elif np.min(signal) == 0:
+        offset_eps = True
+        signal += eps
+    n_samples = len(signal)
+    beta = cvx.Variable()
+    envelope = cvx.Variable(len(signal))
+    mu = cvx.Parameter(sign='positive', value=mu)
+    eta = cvx.Parameter(sign='positive', value=eta)
+    cost = (cvx.sum_entries(cvx.huber(envelope - np.log(signal))) +
+            mu * cvx.norm2(envelope[2:] - 2 * envelope[1:-1] + envelope[:-2]) +
+            eta * cvx.norm1(cvx.max_elemwise(np.log(signal) - envelope, 0)))
+    objective = cvx.Minimize(cost)
+    constraints = [
+        envelope[:n_samples - period] == envelope[period:] + beta
+    ]
+    problem = cvx.Problem(objective, constraints)
+    try:
+        problem.solve(solver='MOSEK')
+    except Exception as e:
+        print(e)
+        print('Trying ECOS solver')
+        problem.solve(solver='ECOS')
+    if offset_eps:
+        signal -= eps
+    if kind == 'upper':
+        return np.exp(envelope.value.A1), np.exp(beta.value)
+    elif kind == 'lower':
+        signal *= -1
+        return -np.exp(envelope.value.A1), np.exp(beta.value)
+
 def masked_smooth_fit_periodic(signal, mask, period, mu, linear_term=False):
     n_samples = len(signal)
     if linear_term:
