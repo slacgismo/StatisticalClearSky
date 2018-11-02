2ef62b595690dbb627add71b4cfe3487eae75688
Bennet Meyers <bennetm@stanford.edu>
2018-11-02 12:21:30 -0700

    automatic detection and correction of time shift / clock errors in data

--- clearsky/utilities.py
+++ clearsky/utilities.py
@@ -5,6 +5,7 @@ This module contains utility functions and classes.
 import cvxpy as cvx
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Python 2.x, 3.x compatibility
 try:
@@ -197,3 +198,77 @@ def lowpass_2d(D, r=25):
     FS_filtered = np.fft.fftshift(np.multiply(np.fft.fftshift(FS), fltr))
     D_filtered = np.abs(np.fft.ifft2(FS_filtered))
     return D_filtered
+
+
+def fix_time_shifts(D, w=30, tol=5e-2, verbose=False):
+    # For each day, weight each time step by the power produced in that period, and take the average. This gives us
+    # the "center" time point for each day, in terms of hours between 0 and 23 inclusive
+    old_err_state = np.seterr(divide='ignore')
+    s1 = np.divide(np.dot(np.linspace(0, 24, D.shape[0]), D), np.sum(D, axis=0))
+    # Apply a sliding-window average filter
+    s2 = np.convolve(s1, np.ones(w), mode='valid') / w
+    # Apply 1D edge finding algorithm
+    ixs = edge_find_1D(s2, tol=tol)
+    if len(ixs) == 0:
+        if verbose:
+            print('No time shifts found')
+        return D
+    ixs = list(map(lambda x: int(x + w / 2), ixs))
+    num = len(ixs)
+    if verbose:
+        print('{} segments found'.format(num + 1))
+        print('index locations: ', ixs)
+    ixs = np.r_[[None], ixs, [None]]
+    A = []
+    for i in range(len(ixs) - 1):
+        avg = np.average(np.ma.masked_invalid(s1[ixs[i]:ixs[i+1]]))
+        A.append(np.round(avg * D.shape[0] / 24))
+    A = np.array(A)
+    rolls = A[0] - A[1:]
+    Dout = np.copy(D)
+    for ind, roll in enumerate(rolls):
+        D_rolled = np.roll(D, int(roll), axis=0)
+        Dout[:, ixs[ind + 1]:] = D_rolled[:, ixs[ind + 1]:]
+    return Dout
+
+
+
+def edge_find_1D(s1, tol=5e-2, ixs=None, ix0=0, w=30, mu=3, debug=False):
+    # Returns the indices of edges in a 1-D array. This algorithm recursively segments the input array until all edges
+    # have been found.
+    if ixs is None:
+        ixs = []
+    x = cvx.Variable(len(s1))
+    mu = cvx.Constant(mu)
+    obj = cvx.Minimize(cvx.norm(s1[np.isfinite(s1)] - x[np.isfinite(s1)]) + mu * cvx.norm1(x[:-1] - x[1:]))
+    prob = cvx.Problem(obj)
+    prob.solve(solver='MOSEK')
+    if debug:
+        plt.plot(x.value)
+        plt.show()
+    s2 = np.abs(x.value[:-1] - x.value[1:])
+    if debug:
+        print(s2.max() - s2.min())
+    if s2.max() - s2.min() < tol:
+        # There are no step shifts in this data segment
+        return ixs
+    else:
+        # There is a step shift in this data segment
+        ix = np.argsort(-s2)[0]
+        vr_best = -np.inf
+        for j in xrange(ix - w, ix + w):
+            jx = max(0, j)
+            jx = min(jx, len(s1))
+            sa = s1[:jx][np.isfinite(s1)[:jx]]
+            sb = s1[jx:][np.isfinite(s1)[jx:]]
+            vr = (np.std(s1[np.isfinite(s1)]) ** 2
+                  - (len(sa) / len(s1[np.isfinite(s1)])) * np.std(sa)
+                  - (len(sb) / len(s1[np.isfinite(s1)])) * np.std(sb))
+            if vr > vr_best:
+                vr_best = vr
+                ix_best = jx
+        ixs.append(ix_best + ix0)
+        ixs1 = edge_find_1D(s1[:ix_best], tol=tol, ixs=ixs, ix0=ix0)
+        ixs2 = edge_find_1D(s1[ix_best:], tol=tol, ixs=ixs1, ix0=ix0+ix_best)
+        ixs2.sort()
+        return ixs2
