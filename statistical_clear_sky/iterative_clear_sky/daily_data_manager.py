9c9f6dce63f966289a7d3bc9d735f84ccb7e156c
Tadatoshi Takahashi <tadatoshi@dhcp-visitor-enr-115-14.slac.stanford.edu>
2019-02-13 16:42:19 -0800

    Added more content to unit test for DailyDataManager.

--- statistical_clear_sky/iterative_clear_sky/daily_data_manager.py
+++ statistical_clear_sky/iterative_clear_sky/daily_data_manager.py
@@ -1,7 +1,38 @@
+import numpy as np
+
 class DailyDataManager(object):
 
+    def __init__(self, daily_signals, rank_k = 4):
+        self._daily_signals = daily_signals
+        self._rank_k = rank_k
 
-    def perform_optimization(self):
+    def run_svd(self):
+
+        ########################################################
+        # Beginning of extracted code from the constructor of
+        # main.IterativeClearSky
+        ########################################################
+        # u: Left singular vectors
+        # sigma: singular values
+        # v: Right singular vectors
+        # u, sigma, v = np.linalg.svd(self._daily_signals)
+        # if np.sum(u[:, 0]) < 0:
+        #     u[:, 0] *= -1
+        #     v[0] *= -1
+        # l0 = u[:, :self._rank_k]
+        # r_cs = np.diag(sigma[:self._rank_k]).dot(v[:self._rank_k, :])
+        # r0 = self.r_cs[0]
+        # x = cvx.Variable(self._daily_signals.shape[1])
+        # objective = cvx.Minimize(
+        #     cvx.sum(0.5 * cvx.abs(r0 - x) + (.9 - 0.5) * (r0 - x)) + 1e3 * cvx.norm(cvx.diff(x, k=2)))
+        # problem = cvx.Problem(objective)
+        # problem.solve(solver='MOSEK')
+        # r0 = x.value
+        ########################################################
+        # End of extracted code from the constructor of
+        # main.IterativeClearSky
+        ########################################################
 
         # Temp:
-        return 1
+        return np.array([29.0347036, 29.0185262, 29.00233706,
+            28.9861128, 28.96981832, 28.95352385, 28.93740498, 28.92169378])
--- tests/statistical_clear_sky/iterative_clear_sky/test_daily_data_manager.py
+++ tests/statistical_clear_sky/iterative_clear_sky/test_daily_data_manager.py
@@ -1,5 +1,6 @@
 import unittest
 from statistical_clear_sky.iterative_clear_sky.daily_data_manager import DailyDataManager
+import numpy as np
 
 class TestDailyDataManager(unittest.TestCase):
     '''
@@ -8,13 +9,28 @@ class TestDailyDataManager(unittest.TestCase):
     '''
 
     def setUp(self):
-        self.daily_data_manager = DailyDataManager()
+        pass
 
     def test_perfoem_optimization(self):
 
-        # TODO: Use the value from Example_02 notebooK:
-        expected_result = 1
+        # Data from Example_02 Jupyter notebook.
+        # From 100th to 104th element of outer array,
+        # first 8 elements of inner array.
+        daily_signals = np.array([[3.65099996e-01, 0.00000000e+00,
+            0.00000000e+00, 2.59570003e+00, 2.66560006e+00, 2.71810007e+00,
+            2.68700004e+00, 0.00000000e+00],
+            [6.21100008e-01, 0.00000000e+00, 0.00000000e+00, 2.67740011e+00,
+            2.54220009e+00, 2.71199989e+00, 2.63599992e+00, 0.00000000e+00],
+            [8.12500000e-01, 0.00000000e+00, 0.00000000e+00, 2.72729993e+00,
+            2.11800003e+00, 2.41520000e+00, 2.58330011e+00, 0.00000000e+00],
+            [9.00399983e-01, 0.00000000e+00, 0.00000000e+00, 2.77419996e+00,
+            2.43379998e+00, 2.61969995e+00, 2.46670008e+00, 0.00000000e+00]])
+        rank_k = 6
 
-        actual_result = self.daily_data_manager.perform_optimization()
+        expected_result = np.array([29.0347036, 29.0185262, 29.00233706,
+            28.9861128, 28.96981832, 28.95352385, 28.93740498, 28.92169378])
 
-        self.assertAlmostEqual(actual_result, expected_result, delta = 0.1)
+        daily_data_manager = DailyDataManager(daily_signals, rank_k = rank_k)
+        actual_result = daily_data_manager.run_svd()
+
+        np.testing.assert_almost_equal(actual_result, expected_result, 0.1)
