e3b6e3743aa50821f2967ed695aab42a519835e3
Tadatoshi Takahashi <tadatoshi@dhcp-visitor-enr-115-14.slac.stanford.edu>
2019-02-20 12:18:23 -0800

    In setup.py, explicitly specified numpy>=1.16 since with the version less than that, cvxpy fails.

--- setup.py
+++ setup.py
@@ -146,7 +146,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'pandas', 'seaborn', 'cvxpy'],  # Optional
+    install_requires=['numpy>=1.16', 'pandas', 'seaborn', 'cvxpy>=1.0'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
