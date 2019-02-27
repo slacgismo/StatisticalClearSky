c261df0bf3715401426e49d638b109db869c2ee2
Tadatoshi Takahashi <tadatoshi@dhcp-visitor-enr-115-14.slac.stanford.edu>
2019-02-27 13:54:24 -0800

    Took care of information hiding with regard to mu_l, mu_r and tau.

--- statistical_clear_sky/algorithm/iterative_clear_sky.py
+++ statistical_clear_sky/algorithm/iterative_clear_sky.py
@@ -4,6 +4,7 @@ This module defines "Statistical Clear Sky Fitting" algorithm.
 
 from time import time
 import numpy as np
+from numpy.linalg import norm
 import cvxpy as cvx
 from statistical_clear_sky.algorithm.initialization.linearization_helper\
  import LinearizationHelper
@@ -22,6 +23,8 @@ class IterativeClearSky(SerializationMixin):
 
     def __init__(self, power_signals_d, rank_k=4, solver_type=SolverType.ecos,
                  reserve_test_data=False, auto_fix_time_shifts=True):
+
+        self._solver_type = solver_type
 
         self._fixed_time_stamps = False
         self._power_signals_d = power_signals_d
@@ -47,39 +50,35 @@ class IterativeClearSky(SerializationMixin):
         self._r_cs.value = np.diag(self._singular_values_sigma[:rank_k]).dot(
             self._right_low_rank_matrix_v[:rank_k, :])
 
-        self._mu_l = 1.
-        self._mu_r = 20.
-        self._tau = 0.8
-
         self._residuals_median = None
         self._residuals_variance = None
         self._residual_l0_norm = None
 
         self._linearization_helper = LinearizationHelper(
-            solver_type=SolverType.ecos)
+            solver_type=self._solver_type)
 
-        self._weight_setting = WeightSetting(solver_type=SolverType.ecos)
+        self._weight_setting = WeightSetting(solver_type=self._solver_type)
+
+        self._set_testdays(power_signals_d, reserve_test_data)
 
         # Stores the current state of the object:
         self._state_data = StateData()
         self._store_initial_state_data()
 
-    def minimize_objective(self, eps=1e-3, max_iter=100, calc_deg=True,
+    def minimize_objective(self, mu_l=1.0, mu_r=20.0, tau=0.8,
+                           eps=1e-3, max_iter=100, calc_deg=True,
                            max_deg=None, min_deg=None,
-                           mu_l=None, mu_r=None, tau=None, verbose=True):
+                           verbose=True):
 
         self._obtain_component_r0()
         self._obtain_weights()
 
-        if mu_l is not None:
-            self._mu_l = mu_l
-        if mu_r is not None:
-            self._mu_r = mu_r
-        if tau is not None:
-            self._tau = tau
+        self._minimization_state_data(mu_l, mu_r, tau)
+
         ti = time()
         try:
-            obj_vals = self._calculate_objective(False)
+            obj_vals = self._calculate_objective(mu_l, mu_r, tau,
+                                                 sum_components=False)
             if verbose:
                 print('starting at {:.3f}'.format(np.sum(obj_vals)), obj_vals)
             improvement = np.inf
@@ -87,11 +86,12 @@ class IterativeClearSky(SerializationMixin):
             it = 0
             f1_last = obj_vals[0]
             while improvement >= eps:
-                if self.test_days is not None:
+                if self._test_days is not None:
                     self._weights[self.test_days] = 0
-                self.min_L()
-                self.min_R(calc_deg=calc_deg, max_deg=max_deg, min_deg=min_deg)
-                obj_vals = self._calculate_objective(sum_components=False)
+                self.min_l()
+                self.min_r(calc_deg=calc_deg, max_deg=max_deg, min_deg=min_deg)
+                obj_vals = self._calculate_objective(mu_l, mu_r, tau,
+                                                     sum_components=False)
                 new_obj = np.sum(obj_vals)
                 improvement = (old_obj - new_obj) * 1. / old_obj
                 old_obj = new_obj
@@ -136,24 +136,24 @@ class IterativeClearSky(SerializationMixin):
                 self._metrix_l0[:, 0] - self._l_cs.value[:, 0]
             )
 
-        self._final_state_data()
+        self._store_final_state_data()
 
-    def _calculate_objective(self, sum_components=True):
+    def _calculate_objective(self, mu_l, mu_r, tau, sum_components=True):
         weights1 = np.diag(self._weights)
         form1 = (cvx.sum((0.5 * cvx.abs(
             self._power_signals_d - self._l_cs.value.dot(self._r_cs.value))
-                              + (self.tau - 0.5) * (self.D - self._l_cs.value.dot(self._r_cs.value))) * weights1)).value
-        weights2 = np.eye(self.k)
-        form2 = self._mu_l * norm(((self._l_cs[:-2, :]).value -
+                              + (tau - 0.5) * (self._power_signals_d - self._l_cs.value.dot(self._r_cs.value))) * weights1)).value
+        weights2 = np.eye(self._rank_k)
+        form2 = mu_l * norm(((self._l_cs[:-2, :]).value -
                                2 * (self._l_cs[1:-1, :]).value +
                                (self._l_cs[2:, :]).value).dot(weights2), 'fro')
-        form3 = self._mu_r * norm((self._r_cs[:, :-2]).value -
+        form3 = mu_r * norm((self._r_cs[:, :-2]).value -
                               2 * (self._r_cs[:, 1:-1]).value +
                               (self._r_cs[:, 2:]).value, 'fro')
         if self._r_cs.shape[1] < 365 + 2:
             form4 = 0
         else:
-            form4 = (self._mu_r * cvx.norm(self._r_cs[1:, :-365] - self._r_cs[1:, 365:], 'fro')).value
+            form4 = (mu_r * cvx.norm(self._r_cs[1:, :-365] - self._r_cs[1:, 365:], 'fro')).value
         components = [form1, form2, form3, form4]
         objective = sum(components)
         if sum_components:
@@ -180,6 +180,16 @@ class IterativeClearSky(SerializationMixin):
         self._weights = self._weight_setting.obtain_weights(
             self._power_signals_d)
 
+    def _set_testdays(self, power_signals_d, reserve_test_data):
+        if reserve_test_data:
+            m, n = power_signals_d.shape
+            day_indices = np.arange(n)
+            num = int(n * reserve_test_data)
+            self._test_days = np.sort(np.random.choice(day_indices, num,
+                                                       replace=False))
+        else:
+            self._test_days = None
+
     def _store_initial_state_data(self):
         self._state_data.power_signals_d = self._power_signals_d
         self._state_data.rank_k = self._rank_k
@@ -188,6 +198,11 @@ class IterativeClearSky(SerializationMixin):
         self._state_data.l_value = self._l_cs.value
         self._state_data.r_value = self._r_cs.value
 
-    def _final_state_data(self):
+    def _minimization_state_data(self, mu_l, mu_r, tau):
+        self._state_data.mu_l = mu_l
+        self._state_data.mu_r = mu_r
+        self._state_data.tau = tau
+
+    def _store_final_state_data(self):
         self._state_data.l_value = self._l_cs.value
         self._state_data.r_value = self._r_cs.value
