b77591eac88609af9e6326f0ed7456b58149d30e
tadatoshi <tadatoshi.takahashi@slac.stanford.edu>
2019-03-28 15:32:53 -0700

    Replaced right matrix minimization test fixture with synthetic data.

--- tests/statistical_clear_sky/algorithm/minimization/test_right_matrix_minimization.py
+++ tests/statistical_clear_sky/algorithm/minimization/test_right_matrix_minimization.py
@@ -12,7 +12,8 @@ class TestRightMatrixMinimization(unittest.TestCase):
 
         input_power_signals_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
-            "../../fixtures/right_matrix_minimization/power_signals_d_1.csv"))
+            "../../fixtures/right_matrix_minimization",
+            "three_years_power_signals_d_1.csv"))
         with open(input_power_signals_file_path) as file:
             power_signals_d = np.loadtxt(file, delimiter=',')
 
@@ -20,7 +21,8 @@ class TestRightMatrixMinimization(unittest.TestCase):
 
         weights_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
-            "../../fixtures/right_matrix_minimization/weights_1.csv"))
+                "../../fixtures/right_matrix_minimization",
+                "three_years_weights.csv"))
         with open(weights_file_path) as file:
             weights = np.loadtxt(file, delimiter=',')
 
@@ -29,21 +31,22 @@ class TestRightMatrixMinimization(unittest.TestCase):
 
         initial_r0_value_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
-            "../../fixtures/right_matrix_minimization/initial_r0_value_1.csv"))
+            "../../fixtures/right_matrix_minimization",
+            "three_years_initial_component_r0.csv"))
         with open(initial_r0_value_file_path) as file:
             initial_component_r0_value = np.loadtxt(file, delimiter=',')
 
         initial_l_cs_value_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
             "../../fixtures/right_matrix_minimization",
-            "initial_l_cs_value_1.csv"))
+            "l_cs_value_after_left_matrix_minimization_iteration_1.csv"))
         with open(initial_l_cs_value_file_path) as file:
             initial_l_cs_value = np.loadtxt(file, delimiter=',')
 
         initial_r_cs_value_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
             "../../fixtures/right_matrix_minimization",
-            "initial_r_cs_value_1.csv"))
+            "r_cs_value_after_left_matrix_minimization_iteration_1.csv"))
         with open(initial_r_cs_value_file_path) as file:
             initial_r_cs_value = np.loadtxt(file, delimiter=',')
 
@@ -52,18 +55,18 @@ class TestRightMatrixMinimization(unittest.TestCase):
         l_cs_value_after_iteration_1_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
             "../../fixtures/right_matrix_minimization",
-            "l_cs_value_after_iteration_1_1.csv"))
+            "l_cs_value_after_right_matrix_minimization_iteration_1.csv"))
         with open(l_cs_value_after_iteration_1_file_path) as file:
             expected_l_cs_value = np.loadtxt(file, delimiter=',')
 
         r_cs_value_after_iteration_1_file_path = os.path.abspath(
             os.path.join(os.path.dirname(__file__),
             "../../fixtures/right_matrix_minimization",
-            "r_cs_value_after_iteration_1_1.csv"))
+            "r_cs_value_after_right_matrix_minimization_iteration_1.csv"))
         with open(r_cs_value_after_iteration_1_file_path) as file:
             expected_r_cs_value = np.loadtxt(file, delimiter=',')
 
-        expected_beta_value = -0.04617361
+        expected_beta_value = -0.04015762
 
         right_matrix_minimization = RightMatrixMinimization(power_signals_d,
             rank_k, weights, tau, mu_r, solver_type=SolverType.mosek)
@@ -81,11 +84,8 @@ class TestRightMatrixMinimization(unittest.TestCase):
         else:
             np.testing.assert_array_equal(actual_l_cs_value,
                                           expected_l_cs_value)
-            # np.testing.assert_array_equal(actual_r_cs_value,
-            #                               expected_r_cs_value)
-            np.testing.assert_almost_equal(actual_r_cs_value,
-                                           expected_r_cs_value,
-                                           decimal=4)
+            np.testing.assert_array_equal(actual_r_cs_value,
+                                          expected_r_cs_value)
             # np.testing.assert_array_equal(actual_beta_value,
             #                               expected_beta_value)
             np.testing.assert_almost_equal(actual_beta_value,
