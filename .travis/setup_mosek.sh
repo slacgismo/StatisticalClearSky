f46f1f78ba403577082467a2c8eba717c15cc6ad
Jonathan Goncalves <jonathan@rhumbix.com>
2019-05-23 13:29:24 -0700

    Let me not spend too much more time on this

--- .travis/setup_mosek.sh
+++ .travis/setup_mosek.sh
@@ -1,16 +1,8 @@
 #!/bin/sh -e
 
-. .travis/utility.sh
+echo "Create the mosek license folder"
+cd $HOME
+mkdir mosek
 
-start_section "Create the mosek license folder"
-    cd $HOME
-    mkdir mosek
-end_section
-
-start_section "Verify the AWS creds are loaded in the environment"
-    check_for_env AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
-end_section
-
-start_section "Pull the mosek license from s3"
-    aws s3 cp s3://slac.gismo.ci.artifacts/mosek.license/mosek.lic $HOME/mosek/mosek.lic
-end_section
+echo "Pull the mosek license from s3"
+aws s3 cp s3://slac.gismo.ci.artifacts/mosek.license/mosek.lic $HOME/mosek/mosek.lic
